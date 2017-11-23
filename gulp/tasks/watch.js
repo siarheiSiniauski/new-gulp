26b4175de29e8e8bfd96bb5cc9f1d735c89919cf
Сергей <siarhei.siniauski@mail.ru>
2017-11-23 03:35:32 +0300

    Update watch.js

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,7 +2,7 @@
 
 module.exports = function() {
     $.gulp.task('watch', function () {
-        $.gulp.watch('./source/style/*.sass', $.gulp.series('sass'));
+        $.gulp.watch('./source/style/**/*.sass', $.gulp.series('sass'));
         $.gulp.watch('./source/template/**/*.pug', $.gulp.series('pug'));
     });
 };
